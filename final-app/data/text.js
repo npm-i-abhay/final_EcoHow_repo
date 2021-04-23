5ced438f83d2e1b56567d1e0aedb9d65ba0ad9e7
Nick Barr <nbarr2@my.bcit.ca>
2021-04-22 20:41:23 -0700

    added data page and began building radio buttons comp

---
+++ final-app/data/text.js
@@ -0,0 +1,174 @@
+
+export const OrganicContent = {
+    Vegetable:{
+        title:"Vegetable",
+        content:"Vegetables, plus roots, and tubers have the highest wastage rates of any food.It is important to know that vegetables contain nitrogen, an ideal chemical compound for  compost. Just remember to bury it deep in the pile to avoid a rodent infestation."
+    },
+    Fruits:{
+        title:"Fruit",
+        content:"Almost 14 700 000 tons of fruits are wasted every year in Canada.  However, fruits contain Nitrogen.Therefore, It is a great addition to your compost.Be sure to avoid citrus as it is too acidic and can kill worms and microorganisms, this can slow down the decomposition in your pile."
+    },
+
+    GardenWaste:{
+        title:"Garden Waste",
+        content:"Garden waste makes up 14% of the average household bin.We should note that garden waste contains a good amount of carbon. As a result, this provides energy to break down materials in your compost.Without garden waste, compost cannot progress effectively. "
+    },
+    Sewage:{
+        title:"Sewage",
+        content:"It takes up to 24 for hours for a single drop of water to be treated in the Treatment Plant Process and be safe for consumption.If you own a pet, you might be interested to know that animal manure is a great add addition to your compost as it is rich in Nitrogren."
+    },
+    Vegetable:{
+        title:"Meat",
+        content:"Producing meat requires excessive amounts of resources, this includes land, water, and energy. With that said, we still should not add meat to your compost since meat may be infected with bacteria like Salmonella and E. coli that can make you ill. Furthermore, as compost material It also attracts pests and rodents. "
+    },
+    Vegetable:{
+        title:"Wood",
+        content:"In the United States alone, it was estimated that 18.1 million tons of wood was wasted in 2018. 90% of that waste was wood palettes for packaging.Wood chips and sawdust contains high levels of carbon which is a great addition to your compost."
+    }
+
+   
+}
+
+
+export const InorganicContent = {
+    Plastic:{
+        title:"Plastic Bags and Wrappers",
+        content:"It is estimated that consumers use 500 billion to 1 trillion bags one year. The issue is that plastic is non-biodegradable. In addition, only 1 to 3% is actually recycled. The rest goes to the landfill, or is littered in the ocean. The good news is there are many alternatives to this material that are more sustainable and reusable, using a fabric bag or pouch instead of purchasing plastic bags when you go to the supermarket can already make a huge difference. "
+    },
+    PlasticStraws:{
+        title:"Plastic Straws",
+        content:"Almost 14 700 000 tons of fruits are wasted every year in Canada.  However, fruits contain Nitrogen.Therefore, It is a great addition to your compost.Be sure to avoid citrus as it is too acidic and can kill worms and microorganisms, this can slow down the decomposition in your pile."
+    },
+
+    Styrofoam:{
+        title:"Styrofoam",
+        content:"Garden waste makes up 14% of the average household bin.We should note that garden waste contains a good amount of carbon. As a result, this provides energy to break down materials in your compost.Without garden waste, compost cannot progress effectively. "
+    },
+    Cardboard:{
+        title:"Cardboard and Paper",
+        content:"It takes up to 24 for hours for a single drop of water to be treated in the Treatment Plant Process and be safe for consumption.If you own a pet, you might be interested to know that animal manure is a great add addition to your compost as it is rich in Nitrogren."
+    },
+    Aluminium:{
+        title:"Aluminium",
+        content:"Producing meat requires excessive amounts of resources, this includes land, water, and energy. With that said, we still should not add meat to your compost since meat may be infected with bacteria like Salmonella and E. coli that can make you ill. Furthermore, as compost material It also attracts pests and rodents. "
+    },
+    Electronics:{
+        title:"Electronics",
+        content:"In the United States alone, it was estimated that 18.1 million tons of wood was wasted in 2018. 90% of that waste was wood palettes for packaging.Wood chips and sawdust contains high levels of carbon which is a great addition to your compost."
+    },
+    
+    Reuse:{
+        title:"Reuse",
+        content:"The average person generates 4.4 pounds of solid waste every day. This needs to change, we all need to look out for our planet. We hope this knowledge can generate momentum. We should all be responsible for handling our waste properly, as it can help our environemnt so much. Remember, consume less and  think abut your purchases often. Always ask youself whether  what you are buying is essential, and whether you can use it for many years to come. "
+    }
+   
+}
+
+export const OrganicHelp = {
+
+    CategorySelection:{
+        content:"The organic sectcion will teach you about composting, we will share insight on different materials that can be added to create balanced compost. Click “enter” to begin"
+    },
+
+    Subcat:{
+        content:"Read each category carefully! This will help you gain the most from using this app.Once you have finished this section and completed your evaluation. You can skip ahead to the quiz"
+    },
+
+    Evaluation:{
+        content:"This evaluation section is intended to help you reflect on your current habits. Reflecting on your results can help you progress and make actionable changes"
+    },
+    Quiz1:{
+        content:"There are a total of three questions. You may click on the hint button at any time to help yourself throughout the quiz. "
+    },
+    Quiz2:{
+        content:"There are a total of three questions. You may click on the hint button at any time to help yourself throughout the quiz."
+    },
+
+    Quiz3:{
+        content:"There are a total of three questions. You may click on the hint button at any time to help yourself throughout the quiz."
+    },
+
+}
+
+
+export const OrganicHints = {
+    Quiz1:{
+        content:"One element creates engergy and the other breaks organic matter. "
+    },
+    Quiz2:{
+        content:"Citrus is very acidic and takes a very long time to decompose."
+    },
+
+    Quiz3:{
+        content:"Do we want rodents and pests in our compost?"
+    },
+
+    OrganicResults:{
+        content:"You have shown that you are very knowledgeable in this category! Great job! If you would like to try the next catagory, click on “Back to Categories”."}
+}
+
+
+export const OrganicTips = {
+    GoodTips:{
+        content:"Click on “Test Your Knowledge” button to continue to quiz"
+    },
+    BadTips:{
+        content:"This evaluation result is suggestive, it is intended to motivate you to keep improving. Even if you are already doing great, there is always room for improvement. "
+    },
+}
+
+export const InorganicHelp = {
+
+    CategorySelection:{
+        content:"The inorganic sectcion will teach you about waste and recycling, we will share facts and insight on domestic waste and how to minimize your indivual impact.  Click “enter” to begin"
+    },
+
+    Subcat:{
+        content:"Read each category carefully! This will help you gain the most from using this app.Once you have finished this section and completed your evaluation. You can skip ahead to the quiz"
+    },
+
+    Evaluation:{
+        content:"This evaluation section is intended to help you reflect on your current habits. Reflecting on your results can help you progress and make actionable changes"
+    },
+    Quiz1:{
+        content:"There are a total of three questions. You may click on the hint button at any time to help yourself throughout the quiz. "
+    },
+    Quiz2:{
+        content:"There are a total of three questions. You may click on the hint button at any time to help yourself throughout the quiz."
+    },
+
+    Quiz3:{
+        content:"There are a total of three questions. You may click on the hint button at any time to help yourself throughout the quiz."
+    },
+
+}
+
+export const InorganicTips = {
+    GoodTips:{
+        content:"Based on your score, you are doing a great job at keeping your personal waste generation at a minimum. You are keeping our environment healthy"
+    },
+    BadTips:{
+        content:"Based on your score, you may want to continue to do research on proper recycling. The canadian overnemnt has a great resource that guides you how to become more knowledgeable. Finally, consider shopping at second-hand stores, this resourceful act cam reduce waste generation "
+    },
+}
+
+
+export const InorganicHints = {
+    Quiz1:{
+        content:"Remember, the blue bin is where we place all our recylable items.  "
+    },
+    Quiz2:{
+        content:"Our priority is to reduce waste, which of the following actions will help us do that"
+    },
+
+    Quiz3:{
+        content:"Remember, three  products mentioned previously have traces of this polymer "
+    },
+
+    InOrganicResults:{
+        content:"You have shown that you are very knowledgeable in this category! Great job! If you would like to try the next catagory, click on “Back to Categories”."}
+}
+
+
+
+
