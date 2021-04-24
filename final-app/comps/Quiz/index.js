c0917558c502d5ea307e84b22a42cd9e47de226a
Nick Barr <nbarr2@my.bcit.ca>
2021-04-24 08:42:20 -0700

    Constructed Quiz Component and made other minor edits

--- final-app/comps/Quiz/index.js
+++ final-app/comps/Quiz/index.js
@@ -1,48 +1,28 @@
 import React from 'react';
+import { MdEdit } from 'react-icons/md';
 import styled from 'styled-components';
 
 
-const QuestionCont = styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-background-color: #F5F1ED;
-width:292px;
-height:58px;
-border-radius:16px;
+const QuizCont = styled.div`
 `;
 
 
-const QuestionText = styled.div`
-font-size:16px;
-padding:20px;
-`;
-
-const MainRadio = styled.input.attrs(props => ({type:"radio"})) `
-
-`
 
 
 
-
-const QuizQuestions = ({
-    question="What came first the chicken or the egg?"
-}) => {
-
-    return <div> 
-        <QuestionCont>
-            <QuestionText>{question}</QuestionText>
-        </QuestionCont>
-        <br></br>
-        <br></br>
-        <MainRadio/>
-        <br></br>
-        <br></br>
-        <MainRadio/>
-        <br></br>
-        <br></br>
-        <MainRadio/>
-</div>
+const RadioComp = ({
+	onClick=()=>{}	//past the radio button value back out
+}) =>{
+	
+	//w3 radio example, pass the click handler back out
+	return <QuizCont>
+		<input type="radio" id="male" name="gender" value="male" onClick={onClick} />
+		<label for="male">Male</label>
+		<input type="radio" id="female" name="gender" value="female" onClick={onClick} />
+		<label for="female">Female</label>
+		<input type="radio" id="other" name="gender" value="other"onClick={onClick} />
+		<label for="other">Other</label>
+	</QuizCont>
 }
 
-export default QuizQuestions;
+export default RadioComp;
